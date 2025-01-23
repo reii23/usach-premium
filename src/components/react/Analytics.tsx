2698148270c701d55f7dcd0b38370b1a6dd25f4b
Jaime Riquelme <jaime.riquelme@usach.cl>
2025-01-23 00:13:34 -0300

    ADD Totems blog

--- src/components/react/Analytics.tsx
+++ src/components/react/Analytics.tsx
@@ -1,5 +1,13 @@
-import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
+// src/components/Analytics.jsx
+import React from "react";
+import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react"; // Asegúrate de importar correctamente
 
 export default function Analytics() {
-  return <VercelAnalytics />;
+  return (
+    <>
+      <VercelAnalytics />
+      <SpeedInsights /> {/* No uses client:load aquí */}
+    </>
+  );
 }
